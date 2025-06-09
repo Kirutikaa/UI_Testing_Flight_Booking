58c7318fda88a8400282b56c0e2caa0f9bccc683
Kirutikaa <kiruthikasrinivasan08@gmail.com>
2025-06-09 12:07:24 +0200

    UI Test Automation - Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# UI Automation Test Project
+
+## Flight Booking - UI Automation test
+
+This UI test automation project is run on the below website.
+
+(https://rahulshettyacademy.com/dropdownsPractise/)
+
+### clean install
+mvn clean install
+
+### Run the test.
+Run the TestRunner class.
---
+++ src/main/java/com/kirutikaa/uitesting/steps/CommonSteps.java
@@ -0,0 +1,35 @@
+package com.kirutikaa.uitesting.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class CommonSteps {
+
+    public static WebDriver webDriver;
+
+    public static JavascriptExecutor javascriptExecutor;
+
+    @BeforeAll
+    public static void stepDriver(){
+        webDriver = new ChromeDriver();
+        webDriver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        webDriver.manage().window().maximize();
+    }
+    @After
+    public static void stepCloser(){
+        webDriver.quit();
+    }
+
+    public static void addOutLine(WebElement webElement){
+        javascriptExecutor.executeScript("arguments[0].style.border='3px solid white'",webElement);
+    }
+
+    public static void removeOutLine(WebElement webElement){
+        javascriptExecutor.executeScript("arguments[0].style.border='none'",webElement);
+
+    }
+}
